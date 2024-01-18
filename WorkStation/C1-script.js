c7ff60c33798253659a2fec963c0f7d5ffcaab13
Rehan Faryad <127966656+RehanfaryadHussan@users.noreply.github.com>
2024-01-18 20:50:57 +0500

    global.js

--- WorkStation/C1-script.js
+++ WorkStation/C1-script.js
@@ -1,4 +1,22 @@
-document.querySelectorAll('.lofwer').forEach((e)=>{
-    let color = Math.floor(Math.random()*16777215);
-    e.style.backgroundColor =  `#${color.toString(16)}`
+document.querySelectorAll('.lofwer').forEach((e) => {
+    let color = Math.floor(Math.random() * 16777215);
+    e.style.backgroundColor = `#${color.toString(16)}`
+})
+
+
+
+let s1 = s('input');
+s1.style.color = `red`;
+con(Object.prototype)
+s('nav ol').hide();
+s('nav ol').show('flex')
+
+s('.c').addEventListener("click",()=>{
+    let pm = document.querySelector('.pm');
+    let r,s;
+    s = window.getSelection();s
+    r = document.createRange();
+    r.selectNodeContents(pm);
+    s.removeAllRanges()
+    s.addRange(r)
 })
